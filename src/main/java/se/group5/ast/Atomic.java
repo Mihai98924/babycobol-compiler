8ef90f2d36e4613360b28b6d5231baf27c5d1840
j.jonkers <j.jonkers@dynalynx.nl>
2025-05-19 17:09:38 +0200

    Made atomic refer to a data element.

--- src/main/java/se/group5/ast/Atomic.java
+++ src/main/java/se/group5/ast/Atomic.java
@@ -1,16 +1,20 @@
 package se.group5.ast;
 
+import se.group5.ast.data.DataElement;
 import se.group5.ast.literal.Literal;
+
+import javax.swing.text.Element;
+import javax.xml.crypto.Data;
 
 public class Atomic implements Node {
     Literal literal;
-    Identifier identifier;
+    DataElement element;
 
     public Atomic(Literal literal) {
         this.literal = literal;
     }
 
-    public Atomic(Identifier identifier) {
-        this.identifier = identifier;
+    public Atomic(DataElement element) {
+        this.element = element;
     }
 }
