8ffd5acc13ce96fbe23381b2936bcf00e4ee3925
Jakub Stepniewski <jakstepniew@gmail.com>
2025-06-08 10:32:26 +0200

    remove unused imports

--- src/main/java/se/group5/ast/statement/Accept.java
+++ src/main/java/se/group5/ast/statement/Accept.java
@@ -5,9 +5,6 @@ import se.group5.ast.Identifier;
 import se.group5.ast.Program;
 import se.group5.ast.procedure.Procedure;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.List;
 import java.util.stream.Collectors;
 
