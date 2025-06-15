b250720981ef34da326033fa124e59d8f2eb6e82
Joris Wouter Jonkers <joriswouter@live.nl>
2025-06-15 17:44:13 +0200

    Implementation for function

--- src/main/java/se/group5/ast/statement/GoTo.java
+++ src/main/java/se/group5/ast/statement/GoTo.java
@@ -1,12 +1,13 @@
 package se.group5.ast.statement;
 
+import se.group5.ast.Function;
 import se.group5.ast.Identifier;
 import se.group5.ast.Program;
 import se.group5.ast.procedure.Procedure;
 
-public record GoTo(String identifier) implements Procedure {
+public record GoTo(Function function) implements Procedure {
     @Override
-    public void execute(Program state) {
-
+    public void execute(Program program) {
+        function.execute(program);
     }
 }
